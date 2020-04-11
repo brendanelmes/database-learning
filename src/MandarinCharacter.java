110c0015105ffbc6d67ab4abbbb4e087dbf8406a
Brendan Elmes <brendanelmes@gmail.com>
2020-04-11 22:17:26 +0100

    Made all variables and methods package private

--- src/MandarinCharacter.java
+++ src/MandarinCharacter.java
@@ -31,7 +31,7 @@ class MandarinCharacter {
         return strokes;
     }
 
-    public String getAlternativeCharacters() {
+    String getAlternativeCharacters() {
         return alternativeCharacters;
     }
 
